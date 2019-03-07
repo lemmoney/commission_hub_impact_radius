427f94e6e5b49b5d57edae619bc46cea6829d539
Hugo Borges - agaelebe <agaelebe@gmail.com>
2019-03-07 16:20:47 -0300

    update commission_hub so we can set basic auth settings

--- lib/commission_hub/impact_radius/endpoints/base.rb
+++ lib/commission_hub/impact_radius/endpoints/base.rb
@@ -4,7 +4,20 @@ module CommissionHub
       class Base < CommissionHub::Endpoint
 
         def call(request_params={})
+          request_params = {
+            basic_auth: {
+              username: username,
+              password: password
+            } }.merge(request_params)
           connection.class.get(full_uri, request_params)
+        end
+
+        def username
+          connection.settings.basic_auth_username
+        end
+
+        def password
+          connection.settings.basic_auth_password
         end
 
       end
--- lib/commission_hub/impact_radius/settings.rb
+++ lib/commission_hub/impact_radius/settings.rb
@@ -1,7 +1,7 @@
 module CommissionHub
   module ImpactRadius
     class Settings
-      attr_accessor :base_uri
+      attr_accessor :base_uri, :basic_auth_username, :basic_auth_password
     end
   end
 end
